806568e9e2045b55985003d442834510c049b647
Davis Snider (RIT Student) <dls7470@ad.rit.edu>
2018-04-21 13:33:29 -0400

    added player class

---
+++ StealthCooking/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState { Waiting, Moving, Interacting }
+
+public class Player : MonoBehaviour
+{
+    private PlayerState state;
+
+    private Vector3 moveStart;
+    private Vector3 moveEnd;
+    private float lerpTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        state = PlayerState.Waiting;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(state == PlayerState.Waiting)
+        {
+            
+        }
+	}
+}
