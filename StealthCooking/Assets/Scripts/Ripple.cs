249f3875f513d8d23992127b91309ca5ec5c6eb8
Dane Sherman (RIT Student) <dms7827@ad.rit.edu>
2018-04-21 16:34:41 -0400

    workin on ripples

---
+++ StealthCooking/Assets/Scripts/Ripple.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ripple : MonoBehaviour {
+
+    private float maxSize=10;
+    private float growSpeed = 0.5f;
+
+    /// <summary>
+    /// start the Ripple Effect
+    /// </summary>
+    /// <param name="maxSize">based on volume of sound</param>
+    public void Begin(float maxSize)
+    {
+        this.maxSize = maxSize;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (transform.localScale.x < maxSize)
+        {
+            transform.localScale += new Vector3(growSpeed, 0, growSpeed);
+        }
+    }  
+}
