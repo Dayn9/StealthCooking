9f1428fa7aad82edf2c90fae906eeba41f1f0fe5
Davis Snider (RIT Student) <dls7470@ad.rit.edu>
2018-04-22 23:25:39 -0400

    pls work

---
+++ StealthCooking/Assets/Scripts/WinButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinButton : MonoBehaviour
+{
+    public void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => LoadLevel());
+    }
+
+    public void LoadLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+    }
+}
